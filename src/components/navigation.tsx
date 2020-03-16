2ed41f0d1021651439e9c365531e4ac188aab426
Justin Rainbow <justin.rainbow@gmail.com>
2020-03-16 16:40:12 -0700

    chore: trigger new build

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -5,16 +5,7 @@ import { AppBar, Toolbar } from '@material-ui/core';
 export default () => (
   <AppBar position="sticky">
     <Toolbar>
-      <nav role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/blog/">Blog</Link>
-          </li>
-        </ul>
-      </nav>
+      <Link to="/">Home</Link>
     </Toolbar>
   </AppBar>
 );
